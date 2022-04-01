de38969b95d01f242d85a7f2c22e2bd40c9a6fcc
wisehero <godjw96@gmail.com>
2022-04-01 19:14:31 +0900

    # Singleton 예시 코드

---
+++ src/designpattern/singleton/MyProgram.java
@@ -0,0 +1,6 @@
+package src.designpattern.singleton;
+
+public class MyProgram {
+    public static void main(String[] args) {
+    }
+}
---
+++ src/designpattern/singleton/SecondPage.java
@@ -0,0 +1,10 @@
+package src.designpattern.singleton;
+
+public class SecondPage {
+
+    private Settings settings = Settings.getSettings();
+
+    public void printSettings() {
+        System.out.println(settings.getDarkMode() + " " + settings.getFontSize());
+    }
+}
