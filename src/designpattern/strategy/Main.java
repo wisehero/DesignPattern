f7940e75e63180b8c18b797c47ef7b4429a0d5ae
wisehero <godjw96@gmail.com>
2022-04-01 20:46:39 +0900

    # 전략 패턴 예시

---
+++ src/designpattern/strategy/Main.java
@@ -0,0 +1,8 @@
+package src.designpattern.strategy;
+
+public class Main {
+    public static void main(String[] args) {
+        MyProgram myProgram = new MyProgram();
+        myProgram.testProgram();
+    }
+}
