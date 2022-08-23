09a05606eff79f65c063426e7f359e59c6253e20
wisehero10 <godjw96@gmail.com>
2022-08-23 18:06:56 +0900

    싱글톤 패턴 단순하게 구현하기

--- src/designpattern/singleton/MyProgram.java
+++
@@ -1,6 +0,0 @@
-package src.designpattern.singleton;
-
-public class MyProgram {
-    public static void main(String[] args) {
-    }
-}
--- src/designpattern/singleton/Settings.java
+++ src/designpattern/singleton/Settings.java
@@ -2,35 +2,17 @@ package src.designpattern.singleton;
 
 public class Settings {
 
-    private Settings() {
-    }
+	private static Settings instance;
 
+	private Settings() {
 
-    private static Settings settings = null;
+	}
 
-    public static Settings getSettings() {
-        if (settings == null) {
-            settings = new Settings();
-        }
-        return settings;
-    }
+	public static Settings getInstance() {
+		if (instance == null) {
+			instance = new Settings();
+		}
 
-    private boolean darkMode = false;
-    private int fontSize = 13;
-
-    public boolean getDarkMode() {
-        return darkMode;
-    }
-
-    public int getFontSize() {
-        return fontSize;
-    }
-
-    public void setDarkMode(boolean _darkMode) {
-        darkMode = _darkMode;
-    }
-
-    public void setFontSize(int _fontSize) {
-        fontSize = _fontSize;
-    }
+		return instance;
+	}
 }
