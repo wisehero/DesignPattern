5f7b4419bca2c5ef93b90f9364469e0b5ce85e91
wisehero10 <godjw96@gmail.com>
2022-09-10 02:27:53 +0900

    프록시 패턴

---
+++ src/designpattern/_12_proxy/_02_after/Client.java
@@ -0,0 +1,9 @@
+package src.designpattern._12_proxy._02_after;
+
+public class Client {
+
+	public static void main(String[] args) {
+		GameService gameService = new GameServiceProxy();
+		gameService.startGame();
+	}
+}
---
+++ src/designpattern/_12_proxy/_02_after/GameService.java
@@ -0,0 +1,5 @@
+package src.designpattern._12_proxy._02_after;
+
+public interface GameService {
+	void startGame();
+}
